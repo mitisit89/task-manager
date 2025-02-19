9e177a038df6a1c805079a18078cacd299241f60
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-02-19 14:30:18 +0300

    feat:add db conn

--- app/db/connection.py
+++ app/db/connection.py
@@ -1,15 +1,15 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from app.core import settings
+from app import settings
 
 engine = create_async_engine(
     settings.SQLALCHEMY_DATABASE_URL,
     echo=True,
 )
 
-AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
 async def get_db():
-    async with AsyncSessionLocal() as session:
+    async with async_session() as session:
         yield session
