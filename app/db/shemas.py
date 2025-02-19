782ed36b096d307b969a72f5edbb30f5c2f79a83
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-02-19 19:41:56 +0300

    chore:update shemas

--- app/db/shemas.py
+++ app/db/shemas.py
@@ -1,21 +1,26 @@
 from typing import final
 
 from pydantic import BaseModel
-
+from typing import Any
 from app.db.enums import TaskStatus, TaskType
 
 
-class TaskBase(BaseModel):
-    type: TaskType
-    payload: str | None = None
+class TaskCreate(BaseModel):
+    task_type: TaskType
+    payload: dict[str, Any]
 
-
-class TaskCreate(TaskBase): ...
+    @final
+    class Config:
+        from_attributes = True
 
 
 class TaskUpdate(BaseModel):
-    payload: str | None = None
+    payload: dict[str, Any]
     status: TaskStatus
+
+
+class SuccessResponse(BaseModel):
+    status: str
 
 
 class TaskResponse(BaseModel):
