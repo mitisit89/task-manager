b34f26b12d433adabf1fa6c75b5cedb140736bc2
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-02-19 19:41:39 +0300

    chore:update settings

--- app/settings.py
+++ app/settings.py
@@ -2,7 +2,6 @@ import os
 from typing import final
 
 
-@final
 class Settings:
     DB_USER: str = os.getenv("DB_USER", "postgres")
     DB_PASSWORD: str = os.getenv("DB_PASSWORD", "postgres")
@@ -10,14 +9,12 @@ class Settings:
     DB_PORT: str = os.getenv("DB_PORT", "5432")
     DB_NAME: str = os.getenv("DB_NAME", "taskdb")
 
-    SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
     REDIS_HOST: str = os.getenv("REDIS_HOST", "redis")
     REDIS_PORT: int = int(os.getenv("REDIS_PORT", "6379"))
-
-    RABBITMQ_URL: str = os.getenv("RABBITMQ_URL", "amqp://guest:guest@rabbitmq/")
-
+    RABBITMQ_URL: str = os.getenv("RABBITMQ_URL", "amqp://guest:guest@localhost:5672")
     TASK_QUEUE: str = "task_queue"
+    # SQLALCHEMY_DATABASE_URL: str = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URL: str = "sqlite+aiosqlite:///./test.db"
 
 
 settings = Settings()
