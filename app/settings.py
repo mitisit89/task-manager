e3b1db35262c964750c8248c13090a2d711db020
mitisit <29955395+mitisit89@users.noreply.github.com>
2025-02-20 12:16:53 +0300

    refactor:update settings

--- app/settings.py
+++ app/settings.py
@@ -3,18 +3,10 @@ from typing import final
 
 
 class Settings:
-    DB_USER: str = os.getenv("DB_USER", "postgres")
-    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "postgres")
-    DB_HOST: str = os.getenv("DB_HOST", "db")
-    DB_PORT: str = os.getenv("DB_PORT", "5432")
-    DB_NAME: str = os.getenv("DB_NAME", "taskdb")
-
-    REDIS_HOST: str = os.getenv("REDIS_HOST", "redis")
-    REDIS_PORT: int = int(os.getenv("REDIS_PORT", "6379"))
+    REDIS_URL: str = os.getenv("REDIS_URL", "redis://localhost:6379")
     RABBITMQ_URL: str = os.getenv("RABBITMQ_URL", "amqp://guest:guest@localhost:5672")
     TASK_QUEUE: str = "task_queue"
-    # SQLALCHEMY_DATABASE_URL: str = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    SQLALCHEMY_DATABASE_URL: str = "sqlite+aiosqlite:///./test.db"
+    SQLALCHEMY_DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./test.db")
 
 
 settings = Settings()
